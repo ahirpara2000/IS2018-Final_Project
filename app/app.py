13bce0cb4b75d40d972a498efee4ef41f2c9c137
ahirpara2000 <ahirpara2000@gmail.com>
2021-05-06 13:36:59 -0400

    Added search bar, user can now search of artist to get their top tracks

--- app/app.py
+++ app/app.py
@@ -1,17 +1,59 @@
-import mysql.connector
-import simplejson as json
-from flask import Flask, Response
-from flask import render_template
-from spotify_api import *
+from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv, find_dotenv
+import os
+import random
+import spotify_api
 
 app = Flask(__name__)
 
-artist_id = '4YRxDV8wJFPHPTeXepOstw'
+load_dotenv(find_dotenv())
+
+# artist list: [Arijit Singh, Armaan Malik, A.R. Rahman]
+
+artist = ['4YRxDV8wJFPHPTeXepOstw',
+          '4IKVDbCSBTxBeAsMKjAuTs',
+          '1mYsTxnqsietFxj1OgoGbG']
+
 
 @app.route('/')
-def index():
-    return render_template('index.html', song_info=get_song_info(artist_id))
+def run():
+    artist_id = random.choice(artist)
+    song_info = spotify_api.get_song_info(artist_id)
+    return render_template(
+        "index.html",
+        len=len(song_info),
+        len2=len(song_info[0]),
+        song_info=song_info,
+        artist_name=spotify_api.get_artist(artist_id)
+    )
 
+
+@app.route('/artist', methods=["GET", "POST"])
+def artist_search():
+    if request.method == "POST":
+        name = request.form.get("a_name")  # get's artist name form html form
+        artist_id = spotify_api.get_artist_id(name)  # pass in artist name and gets artist id
+
+    else:
+        artist_id = random.choice(artist)  # randomly choose an artist
+
+    if (isinstance(artist_id, int) or artist_id == ''):
+        artist_id = random.choice(artist)  # randomly choose an artist
+        song_info = spotify_api.get_song_info(artist_id)  # gets artist info as an array (random aritst)
+        return render_template(
+            "index.html",
+            err_msg=True,  # error message
+            song_info=song_info,  # array
+            artist_name=spotify_api.get_artist(artist_id)  # gets artist's name
+        )
+    else:
+        song_info = spotify_api.get_song_info(artist_id)  # gets artist info as an array (user picked aritst)
+
+        return render_template(
+            "index.html",
+            song_info=song_info,  # array
+            artist_name=spotify_api.get_artist(artist_id)  # gets artist's name
+        )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- app/spotify_api.py
+++ app/spotify_api.py
@@ -1,6 +1,7 @@
 import requests
 import base64
 from urllib.parse import urlencode
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -32,6 +33,13 @@ def get_access_token():
     return access_token
 
 
+# get_song_info takes in artist's spotify id as a parameter
+# and returns an array of of following format:
+# arr[0] : list of artists
+# arr[1] : name if the song/track
+# arr[2] : preview url of the song/track
+# arr[3] : image related to song
+
 def get_song_info(artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks" + "?" + "market=US"
 
@@ -58,3 +66,56 @@ def get_song_info(artist_id):
         count += 1
 
     return song_info
+
+
+# get_artist takes in artist's spotify id as a parameter
+# and returns name of the artist as a string
+
+def get_artist(artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    method = "GET"
+    request_header = {
+        "Authorization": f"Bearer {get_access_token()}"
+    }
+    response = requests.get(url, headers=request_header)
+    results = response.json();
+
+    artist_info = results['name']
+
+    return artist_info
+
+
+# get_artist_id takes artist name as a parameter
+# and returns artist id if found
+# or returns error code if artist is not found
+
+def get_artist_id(name):
+    url = "https://api.spotify.com/v1/search"
+    mehod = "GET"
+
+    request_header = {
+        "Authorization": f"Bearer {get_access_token()}"
+    }
+
+    # makes a url ready query paramater
+
+    query_param = urlencode({
+        "q": name,
+        "type": "artist",
+        "limit": "1"
+    })
+
+    url_lookup = f"{url}?{query_param}"  # creates a lookup url
+
+    response = requests.get(url_lookup, headers=request_header)  # makes the request
+
+    # if the request is in jason format,
+    # and it contains Artist Id, then the artist id is returns as a string
+    # else a status code is returned as an int
+
+    try:
+        results = response.json()
+        return results["artists"]["items"][0]["id"]
+    except:
+        results = response.status_code
+        return results
